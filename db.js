2bbd02ba7b3b44bc1cc2c35196f3967c1c0723cc
SourabhMurgod <murgodsourabh@gmail.com>
2024-02-17 22:34:28 +0530

    Changed DB endpoint

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql');
 
 const db = mysql.createConnection({
-  host: 'database-test-public.cxue6sqakt87.us-east-2.rds.amazonaws.com',
+  host: 'database-1.cjgucu28qe41.us-east-2.rds.amazonaws.com',
   user: 'admin',
   // password: process.env.DB_KEY,
   password: 'admin123456',
